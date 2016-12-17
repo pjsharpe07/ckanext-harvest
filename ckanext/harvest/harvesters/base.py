836fc09432c80a94992db2038c3028a5a7275f80
fxia <fxia@reisystems.com>
2016-12-17 02:24:45 -0500

    prevent harvest from getting stuck by encode message utf-8

--- ckanext/harvest/harvesters/base.py
+++ ckanext/harvest/harvesters/base.py
@@ -80,7 +80,12 @@ class HarvesterBase(SingletonPlugin):
             Session.rollback()
             err.save()
         finally:
-            log_message = '{0}, line {1}'.format(message,line) if line else message
+            try:
+                log_message = '{0}, line {1}'.format(message,
+                        line) if line else message
+            except UnicodeEncodeError:
+                log_message = '{0}, line {1}'.format(message.encode('utf-8'),
+                        line) if line else message.encode('utf-8')
             log.debug(log_message)
 
     def _get_user_name(self):
